95a16b6822fa389bdf06c4ac51894966bf85a80e
BolotinDaniil <bolotin_daniil522@mail.ru>
2017-04-06 21:56:19 +0300

    Relay race

---
+++ RaW5/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RaW5
+{
+    class Program
+    {
+        static int iteration = 0, NI = 50;
+        static Semaphore e, r, w;
+        static int nr, nw, dr , dw;
+        static int NR = 3, NW = 2;
+        static bool verbose = false;
+
+        static void Main(string[] args)
+        {
+            e = new Semaphore(1, 1);
+            r = new Semaphore(1, 1);
+            w = new Semaphore(1, 1);
+
+            r.WaitOne();
+            w.WaitOne();
+            nr = nw = dr = dw = 0;
+            for (int i = 1; i <= NR; i++)
+            {
+                Thread Reader = new Thread(new ParameterizedThreadStart(ToRead));
+                Reader.Name = "Reader " + i;
+                Reader.Start(i);
+
+            }//for
+            for (int i = 1; i <= NW; i++)
+            {
+                Thread Writer = new Thread(new ParameterizedThreadStart(ToWrite));
+                Writer.Name = "Writer " + i;
+                Writer.Start(i);
+            }//for
+        }//Main
+
+        static void do_somethink(int len)
+        {
+            double b = 1;
+            for (int i = 0; i < len * 1123456; i++)
+                b = 1 / b + i * 1.1;
+        }
+
+        static void signal()
+        {
+            if (verbose) Console.WriteLine("Signal work");
+            if (nw == 0 && dr > 0)
+            {
+                dr--;
+                r.Release(1);
+            }
+            else if (nr == 0 && nw == 0 && dw > 0)
+            {
+                dw--;
+                w.Release(1);
+            }
+            else e.Release(1);
+        }
+        static void ToRead(object num)
+        {
+            while (iteration < NI)
+            {
+                e.WaitOne();
+                if (verbose) Console.WriteLine("Reader {0} begins", num);
+                if (nw > 0)
+                {
+                    dr++;
+                    e.Release(1);
+                    r.WaitOne();
+                }
+                if (verbose) Console.WriteLine("Reader {0} in rw1", num);
+                nr++;
+                signal();
+                do_somethink(7);
+                iteration++;
+
+                if (verbose) Console.WriteLine("Reader {0} goes out", num);
+                e.WaitOne();
+                nr--;
+                signal();
+                Thread.Sleep(100);
+            }//while
+
+            Console.WriteLine("Reader {0} STOP", num);
+            Console.WriteLine(iteration);
+        }//Reader
+
+        static void ToWrite(object num)
+        {
+            while (iteration < NI)
+            {
+                if (verbose) Console.WriteLine("Writer {0} begins", num);
+                e.WaitOne();
+                if (nr > 0 || nw > 0)
+                {
+                    dw++;
+                    e.Release(1);
+                    w.WaitOne();
+                }
+                nw++;
+                if (verbose) Console.WriteLine("Writer {0} in rw1", num);
+                signal();
+                do_somethink(15);
+                iteration++;
+
+                e.WaitOne();
+                nw--;
+                signal();
+                if (verbose) Console.WriteLine("Writer {0} goes out", num);
+                Thread.Sleep(200);
+            }//while
+            Console.WriteLine("Writer {0} STOP", num);
+            Console.WriteLine(iteration);
+        }//Writer
+
+    }
+}
---
+++ RaW5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RaW5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RaW5")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bafda591-5b14-4c97-abe8-7673f8c3b1ca")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
